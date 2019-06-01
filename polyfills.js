32eeee6ac90c6ded1c4e540c005b2819dad59d21
Bhavdip <talaviya.bhavdip@gmail.com>
2019-06-01 23:37:42 +0530

    add portfolio and testimonials

--- polyfills.js
+++ polyfills.js
@@ -5788,8 +5788,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! H:\Portfolio\src\polyfills.ts */"./src/polyfills.ts");
-module.exports = __webpack_require__(/*! H:\Portfolio\node_modules\@angular-devkit\build-angular\src\angular-cli-files\models\jit-polyfills.js */"./node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/jit-polyfills.js");
+__webpack_require__(/*! H:\bhavdip extra\Angular Material Bhavdip\portfolio\src\polyfills.ts */"./src/polyfills.ts");
+module.exports = __webpack_require__(/*! H:\bhavdip extra\Angular Material Bhavdip\portfolio\node_modules\@angular-devkit\build-angular\src\angular-cli-files\models\jit-polyfills.js */"./node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/jit-polyfills.js");
 
 
 /***/ })
--- styles.js
+++ styles.js
@@ -557,7 +557,7 @@ if(false) {}
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! H:\Portfolio\src\styles.css */"./src/styles.css");
+module.exports = __webpack_require__(/*! H:\bhavdip extra\Angular Material Bhavdip\portfolio\src\styles.css */"./src/styles.css");
 
 
 /***/ })
